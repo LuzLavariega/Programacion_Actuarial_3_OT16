11765fb210f0951683a7159da5f97d95f898fe88
Luz Lavariega <lunelaven@hotmail.com>
2016-08-31 09:43:42 -0500

    Se agregó una primer base de datos
    Descargué de Yahoo finance la base de datos de S&P y se utilizó para
    ejemplificar el uso de read.table

--- Avance de clase.R
+++ Avance de clase.R
@@ -25,3 +25,64 @@ m2 <- cbind(m,x)
 m2
 rbind(m1,y)
 cbind(m2,y)
+
+
+#Factores
+
+x <- factor(c("si","no","si","no","si","no"),levels = c("si","no"))
+x
+table(x)
+unclass(x)
+
+x <- factor(c("azul","azul","rojo","azul","amarillo","verde","azul"))
+x
+table(x)
+
+
+#Valores Faltantes
+
+x <- c(1,2,NA,10,3)
+is.na(x)
+is.nan(x)
+
+x <- c(1,2,NaN,10,3)
+is.na(x)
+is.nan(x)
+
+
+#Data Frames
+
+x <- data.frame(Erick = 1:4 , Lori = c(T,T,F,F))
+row.names(x) <- c("Primero","segundo","tercero","cuarto")
+x
+nrow(x)
+ncol(x)
+attributes(x)
+names(x) <- c("Yarely","Karen")
+x
+#Los nombres no son exclusivos de los data frames
+x <- 1:3
+names(x) #Null
+names(x) <- c("Hugo","Paco","Luis")
+x
+
+x <- list(a=1:10,b=100:91,c=51:60)
+x
+names(x) <- c("Seq1","Seq2","Seq3")
+x
+
+m <- matrix(1:4,2,2)
+m
+attributes(m)
+dimnames(m) <- list(c("fil1","fil2"),c("col1","col2"))
+m
+
+
+#lectura de datos
+
+getwd()
+setwd("~/7 Semestre/Progra/Programacion_Actuarial_3_OT16")
+data <- read.csv("Datos_S&P.csv")
+data <- read.table("Datos_S&P.csv",T,",")
+data 
+
